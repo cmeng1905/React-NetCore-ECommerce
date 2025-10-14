6f94d84a6a727c02b05fcd19b951397b81044d84
cem.biber <cem.biber@dmo.gov.tr>
2025-10-14 10:49:50 +0300

    Order entity eklendi.

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,6 +10,8 @@ namespace API.Data
 
         public DbSet<Cart> Carts { get; set; }
 
+        public DbSet<Order> Orders { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
