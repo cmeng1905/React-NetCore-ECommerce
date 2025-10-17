d365bf4b0a7ba32eb7ebd7bdb70d7e1583a05ea4
cem.biber <cem.biber@dmo.gov.tr>
2025-10-17 10:29:57 +0300

    checkout sayfasına stepper eklendi.

--- Client/src/features/checkout/ChekoutPage.tsx
+++ Client/src/features/checkout/ChekoutPage.tsx
@@ -1,11 +1,34 @@
-import { Grid, Paper } from '@mui/material'
-import React from 'react'
+import { Box, Button, Grid, Paper, Step, StepLabel, Stepper } from '@mui/material'
+import React, { act, use, useState } from 'react'
 import Info from './Info'
 import AddressForm from './AddressForm'
 import PaymentForm from './PaymentForm'
 import Review from './Review'
+import { get, set } from 'react-hook-form'
+import { ChevronLeft, ChevronLeftRounded, ChevronRightRounded } from '@mui/icons-material'
+
+const steps = ["Teslimat Bilgileri", "Ödeme Bilgileri", "Sipariş Özeti"];
+function getStepContent(step: number) {
+    switch (step) {
+        case 0:
+            return <AddressForm />
+        case 1:
+            return <PaymentForm />
+        case 2:
+            return <Review />
+        default:
+            throw new Error("Bilinmeyen bir step");
+    }
+}
 
 export default function ChekoutPage() {
+    const [activeStep, setActiveStep] = useState(0);
+    function nextStep() {
+        setActiveStep(activeStep + 1);
+    }
+    function backStep() {
+        setActiveStep(activeStep - 1);
+    }
     return (
         <Paper>
             <Grid container sx={{ p: 4 }}>
@@ -13,9 +36,46 @@ export default function ChekoutPage() {
                     <Info />
                 </Grid>
                 <Grid size={8}>
-                    <AddressForm />
-                    <PaymentForm />
-                    <Review />
+                    <Box>
+                        <Stepper activeStep={activeStep} sx={{ height: 40 }}>
+                            {
+                                steps.map((label, index) => {
+                                    return (
+                                        <Step key={label}>
+                                            <StepLabel>{label}</StepLabel>
+                                        </Step>
+                                    )
+                                })
+                            }
+                        </Stepper>
+                    </Box>
+                    <Box>
+                        {activeStep == steps.length ? (
+                            <>
+                                <h2>Sipariş tamamlandı.</h2>
+                            </>
+                        ) : (
+                            <>
+                                {getStepContent(activeStep)}
+
+                                <Box sx={[
+                                    { display: "flex", mt: 2, mb: 2 },
+                                    activeStep !== 0 ? { justifyContent: "space-between" } : { justifyContent: "flex-end" }
+                                ]}>
+                                    {
+                                        activeStep !== 0 && <Button startIcon={<ChevronLeft />} variant='contained' onClick={backStep}>
+                                            Geri
+                                        </Button>
+                                    }
+                                    <Button startIcon={<ChevronRightRounded />} variant='contained' onClick={nextStep}>
+                                        İleri
+                                    </Button>
+                                </Box>
+                            </>
+
+                        )}
+
+                    </Box>
                 </Grid>
             </Grid>
         </Paper>
